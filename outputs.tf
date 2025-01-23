1391373b5ca7b2486f54e0d3c25f3c0b78854a79
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-01-23 21:06:13 +0530

    vpc module

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,16 @@ output "vpc_id" {
 output "igt_id" {
   value = aws_internet_gateway.igt.id
 }
+
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
